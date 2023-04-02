e48f1eff274f17ecebeea3ace67910a2dd4cb01b
Lucas De Gaudio <lucas.degaudio@live.com>
2023-04-02 16:29:34 -0300

    Fix response format

--- src/helpers/response/response-helper.ts
+++ src/helpers/response/response-helper.ts
@@ -1,10 +1,10 @@
 import { Response } from 'express';
-import { camelCaseToSnakeCase } from './camel-to-snake-case-helper';
+import { objectToSnake } from 'ts-case-convert';
 
 export const resolveResponse = (
   res: Response,
   statusCode: number,
   response: { [key: string]: any },
 ) => {
-  res.status(statusCode).json(camelCaseToSnakeCase(response));
+  res.status(statusCode).json(objectToSnake(response));
 };
